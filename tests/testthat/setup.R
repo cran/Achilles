500c146e9c45d8e353dd982d33508459d8f994c6
Frank DeFalco <fdefalco@ohdsi.org>
2023-05-03 17:15:35 +0000

    version 1.7.1

---
+++ tests/testthat/setup.R
@@ -0,0 +1,18 @@
+library(DatabaseConnector)
+
+if (dir.exists(Sys.getenv("DATABASECONNECTOR_JAR_FOLDER"))) {
+  jdbcDriverFolder <- Sys.getenv("DATABASECONNECTOR_JAR_FOLDER")
+} else {
+  jdbcDriverFolder <- "~/.jdbcDrivers"
+  dir.create(jdbcDriverFolder, showWarnings = FALSE)
+  DatabaseConnector::downloadJdbcDrivers("all", pathToDriver = jdbcDriverFolder)
+  
+  Sys.setenv(DATABASECONNECTOR_JAR_FOLDER=jdbcDriverFolder)
+  
+  withr::defer(
+    {
+      unlink(jdbcDriverFolder, recursive = TRUE, force = TRUE)
+    },
+    testthat::teardown_env()
+  )
+}
